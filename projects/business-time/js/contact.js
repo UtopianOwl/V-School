1a3fdb893f12f0b10b2891611a107f4f4e200f90
Drew Nauman <din3@cornell.edu>
2016-05-11 16:59:53 -0600

    completed business time project.

---
+++ projects/business-time/js/contact.js
@@ -0,0 +1,3 @@
+function thankYou() {
+    alert("Thank you for your submission " + document.contactForm.name.value + ".\n We will be reaching out to you shortly through the email address provided.");
+}
